55c0ee5995963ce0deb7c71f1bf189b82de4e2f3
mtlawder <mtlawder@gmail.com>
2019-06-20 22:45:02 -0400

    Updating main page, adding vue menu
    Making the menu bar into a broken out Vue component and reorganizing the main page

--- static/components/menu.vue
+++ static/components/menu.vue
@@ -1,9 +1,34 @@
-      Vue.component('menu-bar',{
-          data: function() {
-            return{
-              count:2
-            }
-          },
-          template: '<nav class="navbar navbar-default"><div class="container-fluid"><!-- Brand and toggle get grouped for better mobile display --><div class="navbar-header"> <!-- Collect the nav links, forms, and other content for toggling --><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"><ul class="nav navbar-nav"><li><a href="\index_Main"> STLAnnex Home </a></li><li><a href="\all_airlines#">Airline Project </a></li></ul></div><!-- /.navbar-collapse --></div><!-- /.container-fluid --></nav>'
-        }),
-      new Vue({ el: '#menu-vue' })
+Vue.component('menu-bar',{
+  data: function() {
+    return{
+      count:2
+    }
+  },
+  template: '<nav class="navbar navbar-default">\
+    	<div class="container-fluid">\
+        <!-- Brand and toggle get grouped for better mobile display -->\
+        <div class="navbar-header">\
+          <!-- Collect the nav links, forms, and other content for toggling -->\
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+            <ul class="nav navbar-nav">\
+              <li>\
+                <a href="\index_Main"> STLAnnex Home </a>\
+              </li>\
+              <li>\
+                <a href="\all_airlines#">Airline Project </a>\
+              </li>\
+              <li>\
+                <a href="http://miso-epat.herokuapp.com">Wind Energy Project</a>\
+              </li>\
+              <li>\
+                <a href="\#blog_head">Blog Posts</a>\
+              </li>\
+            </ul>\
+          </div>\
+        <!-- /.navbar-collapse -->\
+        </div>\
+      <!-- /.container-fluid -->\
+      </div>\
+    </nav>'
+}),
+new Vue({ el: '#menu-vue' })

c816de856c0b073ea1be1efefd3c726d18bf42e4
mtlawder <mtlawder@gmail.com>
2019-09-05 12:03:47 -0400

    Updating menu
    Menu will now collapse and work better for mobile. Other files need to be updated with new head_imports file

--- static/components/menu.vue
+++ static/components/menu.vue
@@ -4,38 +4,33 @@ Vue.component('menu-bar',{
       count:2
     }
   },
-  template: '<nav class="navbar navbar-default">\
-    	<div class="container-fluid">\
-        <!-- Brand and toggle get grouped for better mobile display -->\
-        <div class="navbar-header">\
-          <!-- Collect the nav links, forms, and other content for toggling -->\
-          <div class="" id="navbarSupportedContent">\
-            <ul class="nav navbar-nav">\
-              <li>\
-                <a href="\index_Main"> STLAnnex Home </a>\
-              </li>\
-              <li>\
-                <a href="\all_airlines#">Airline Project </a>\
-              </li>\
-              <li>\
-                <a href="http://miso-epat.herokuapp.com">Wind Energy Project</a>\
-              </li>\
-              <li>\
-                <a href="\stl_pop">STL Census</a>\
-              </li>\
-              <li>\
-                <a href="\index_Main#blog_head">Blog Posts</a>\
-              </li>\
-              <li>\
-                <a href="\about">About</a>\
-              </li>\
-            </ul>\
-          </div>\
-        <!-- /.navbar-collapse -->\
-        </div>\
-      <!-- /.container-fluid -->\
-      </div>\
-    </nav>'
+  template: '<nav class="navbar navbar-expand-lg navbar-light bg-light">\
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+    <span class="navbar-toggler-icon"></span>\
+  </button>\
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+    <ul class="navbar-nav mr-auto">\
+      <li class="nav-item">\
+        <a class="nav-link" href="\index_Main"> STLAnnex Home </a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="\all_airlines#">Airline Project </a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="http://miso-epat.herokuapp.com">Wind Energy Project</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="\stl_pop">STL Census</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="\index_Main#blog_head">Blog Posts</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="\about">About</a>\
+      </li>\
+    </ul>\
+  </div>\
+</nav>'
 
 
 }),

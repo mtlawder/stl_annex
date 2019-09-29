d32cba52c81ae41ec021db4c091c520e7ec22dcd
mtlawder <mtlawder@gmail.com>
2019-09-28 20:42:25 -0400

    Adding Frontier btn at STL post
    Adding charts and main blog post for frontier at STL by the numbers

--- app.py
+++ app.py
@@ -317,6 +317,10 @@ def about():
 def delta_stl_by_the_numbers():
     return render_template('/by_the_numbers/delta_STL_btn.html')
 
+@app.route('/frontier_stl_by_the_numbers',methods=['GET','POST'])
+def frontier_stl_by_the_numbers():
+    return render_template('/by_the_numbers/frontier_STL_btn.html')
+
 @app.route('/btn_methodology',methods=['GET','POST'])
 def btn_methodology():
     return render_template('/by_the_numbers/btn_methodology.html')
